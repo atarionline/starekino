57d2a9ef205cf40ae771d06d991cccd4c61e23ee
MADRAFi <madrafi@gmail.com>
2020-08-09 18:21:32 +0200

    dodanie nowej klatki kota

--- app/wsk.pas
+++ app/wsk.pas
@@ -1,6 +1,7 @@
 program wsk;
 { $librarypath '../blibs/'}
-uses atari, crt, rmt; // b_utils;
+{$librarypath '../../MADS/blibs/'}
+uses atari, crt, rmt, b_system;
 
 const
 {$i const.inc}
@@ -29,29 +30,44 @@ begin
     hscrol := (x and 3) xor 3;
 end;
 
-begin
-(*  set and run vbl interrupt *)
-    GetIntVec(iVBL, old_vbl);
-    GetIntVec(iDLI, old_dli);
 
-    music:=false;
+begin
+    (*  save vbl and dli *)
+    // GetIntVec(iVBL, old_vbl);
+    // GetIntVec(iDLI, old_dli);
+    // SystemOff;
+
     (*  initialize RMT player  *)
     msx.player := pointer(RMT_PLAYER_ADDRESS);
     msx.modul := pointer(RMT_MODULE_ADDRESS);
     msx.Init(0);
 
+    (*  set and run vbl interrupt *)
     SetIntVec(iVBL, @vbl);
-    SetIntVec(iDLI, @dli);
+    // SetIntVec(iDLI, @dli);
+    // EnableVBLI(@vbl);
 
-    Pause;
+    pause;
+    // waitframe;
     SDLSTL := DISPLAY_LIST_ADDRESS;
+    // DLISTL := DISPLAY_LIST_ADDRESS;
     color4:=$f;
     color1:=$f;
     color2:=0;
+
+    music:=false;
 
     for hpos:=0 to 339 do begin 
         setBackgroundOffset(hpos);
         pause;
     end;
     ReadKey;
+
+    music:= false;
+    msx.stop;
+    // waitframe;
+    // DisableDLI;
+    // DisableVBLI;
+    nmien:=0;
+    Dmactl:= 0;
 end.
